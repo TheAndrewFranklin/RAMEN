dbbc0b4a9be210fd42596f6005091b6cac3b1928
theandrewfranklin <theandrewfranklin@gmail.com>
2021-03-05 15:46:48 -0800

    builds for web, android, ios

--- frontend/src/components/ExploreContainer.tsx
+++ frontend/src/components/ExploreContainer.tsx
@@ -8,11 +8,7 @@ class ExploreContainer extends React.Component {
 				<strong>Ready to create an app?</strong>
 				<p>
 					Start with Ionic{' '}
-					<a
-						target="_blank"
-						rel="noopener noreferrer"
-						href="https://ionicframework.com/docs/components"
-					>
+					<a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">
 						UI Components
 					</a>
 				</p>
